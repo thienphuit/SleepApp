223166e3e2e5ea2326c78c6786b66a7fba16dbb0
thienphuit <thienphua7@gmail.com>
2020-09-30 14:45:43 +0700

    fix reactotron

--- index.js
+++ index.js
@@ -5,5 +5,11 @@ import 'react-native-gesture-handler'
 import { AppRegistry } from 'react-native'
 import App from './App'
 import { name as appName } from './app.json'
-
+// eslint-disable-next-line no-undef
+if (__DEV__) {
+  import('./src/configs/ReactotronConfig').then(() => console.log('Reactotron Configured'))
+} else {
+  console.tron = { log: () => true }
+  // Tranh loi crash khi release co reactrons
+}
 AppRegistry.registerComponent(appName, () => App)
